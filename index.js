c3a9657d576d285329c211d53db303312d94bc01
Swaraj Puppalwar <79976106+UltronTheAI@users.noreply.github.com>
2022-10-08 04:49:07 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,28 +3,44 @@ const app = express();
 const fs = require("fs");
 const upload = require("express-fileupload");
 require('dotenv').config();
-
+console.log("You'r webApp is ready: http://" + process.env.IPV4 + ':5000')
+console.log("Share this link to you'r sender's.\n\n")
 app.use(upload())
 app.use(express.static('public'))
-app.post('/send', (req, res) => {
+app.post('/upload', (req, res) => {
     if (req.files) {
-        var files = req.files.file
-        Object.keys(files).map( key => {
-            var file = files[key];
-            var date = new Date();
-            console.log('###########"' + file.name + '"::' + date.toString() + '#############')
-            file.mv('./upload/' + file.name, (err) => {
-                if (err) {
-                    res.send(err)
-                } else {
-                    date = new Date();
+        if (req.files.file) {
+            var files = req.files.file;
+            if (files.length > 1) {
+                Object.keys(files).map(key => {
+                    var file = files[key];
+                    var date = new Date();
                     console.log('###########"' + file.name + '"::' + date.toString() + '#############')
-                }
-            })
-        })
-        res.redirect('/out.html')
-    } else {
-        res.redirect('/error.html')
+                    file.mv('./upload/' + file.name, (err) => {
+                        if (err) {
+                            res.send(err)
+                        } else {
+                            date = new Date();
+                            console.log('###########"' + file.name + '"::' + date.toString() + '#############')
+                        }
+                    })
+                })
+            } else {
+                var date = new Date();
+                console.log('###########"' + files.name + '"::' + date.toString() + '#############')
+                files.mv('./upload/' + files.name, (err) => {
+                        if (err) {
+                            res.send(err)
+                        } else {
+                            date = new Date();
+                            console.log('###########"' + files.name + '"::' + date.toString() + '#############')
+                        }
+                    })
+            }
+            res.redirect('/out.html')
+        } else {
+            res.redirect('/error.html')
+        }
     }
 });
 app.listen(5000, process.env.IPV4)
